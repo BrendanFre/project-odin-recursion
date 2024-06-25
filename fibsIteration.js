e942d2bc92f9fd28a940c6eb4b05bb0f77ef46f6
Brendan Freeman <80400320+BrendanFre@users.noreply.github.com>
2024-06-25 15:40:08 +0100

    Update fibsIteration.js
    This time with no assistance.

--- fibsIteration.js
+++ fibsIteration.js
@@ -1,15 +1,27 @@
-const fibsIteration = (n) => {
+const { log } = require("console");
+
+const fibs = (n) => {
   let arr = [];
-  let previousPreviousNumber;
-  let previousNumber = 0;
-  let currentNumber = 1;
-  for (let index = 0; index < n; index++) {
-    arr.push(currentNumber);
-    previousPreviousNumber = previousNumber;
-    previousNumber = currentNumber;
-    currentNumber = previousPreviousNumber + previousNumber;
+  let previousprevious = 0;
+  let previous = 0;
+  let current = 1;
+  for (let i = 0; i < n; i++) {
+    if (i == 0) {
+      previousprevious = 0;
+      previous = 0;
+      current = 0;
+    } else if (i == 1) {
+      previousprevious = 0;
+      previous = 0;
+      current = 1;
+    } else {
+      current = previous + current;
+      previousprevious = previous;
+      previous = current - previous;
+    }
+    arr.push(current);
   }
   return arr;
 };
 
-console.log(fibsIteration(3));
+log(fibs(8));
